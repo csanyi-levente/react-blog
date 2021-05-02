1fac0cc020a27cf6ff2f3adfa0c9a32126610144
Csányi Levente <csanyi.levente@greenformatics.hu>
2021-05-02 19:46:20 +0200

    create problems

--- moderation/index.js
+++ moderation/index.js
@@ -6,12 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
+    console.log("Event Recieved! " + req.body );
     const { type, data } = req.body;
 
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
         console.log("Moderated status is: " + status);
-        axios.post('http://localhost:4005/events', {
+        axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentModerated',
             data: {
                 id: data.id,
